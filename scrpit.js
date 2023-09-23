7c61ec15bbedc5163b2d1ecacf67b94c96b6b70f
RaphaelPRZZ <145431036+RaphaelPRZZ@users.noreply.github.com>
2023-09-23 22:14:10 +0200

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,34 @@
+"use scrict"
+
+
+/*
+- La quantité de clones que peut faire Naruto ? Number
+
+- 3 défauts à donner à l'embauche (#perfectionniste)  Tableau
+
+- Le prénom Anne dans la Flamme ? String
+
+- Un panier sur Amazon ? Tableau
+
+- Durée en années de la guerre de 100 ans ? String
+
+- Caractéristiques d'une bière ? Tableau
+
+- Le nom du prochain président de la République ? String
+
+- Le nombre d'Avengers ? Number
+
+- Le nom de famille de l'empereur romain Jules ? String
+
+- Le nombre d'étoiles dans l'univers ? Number
+
+- Message de bienvenue sur un paillasson ? String
+
+- Présence d'un Face ID sur un téléphone ? Boolean
+
+- Prix total d'un ordinateur monté à la main ? Number 
+
+- Noms des 7 nains de Blanche Neige ? Tableau
+
+- Profil d'un beau gosse sur Tinder ? Boolean
+*/
